998957b7da8af861163f90414c12e0acf898c1fe
gilmae <avocadia@fastmail.fm>
2020-12-06 16:47:36 +1100

    Complete day 6

--- day06.rb
+++ day06.rb
@@ -4,11 +4,31 @@ data = get_data("#{__FILE__}".gsub(/\.rb/, ".input")).join("")
 
 groups = data.split("\n\n")
 
-yeses = groups.map { | g |
-    g.gsub(/[^a-z]/,"").chars.uniq.length
-}
+# Count of distinct answers after unioning all member's answers
+def part1 groups
+    yeses = groups.map { | g |
+        g.gsub(/[^a-z]/,"").chars.uniq.length
+    }
 
-puts yeses.reduce(0){ |m,v|
-    m+v
-}
+    return yeses.reduce(0){ |m,v|
+        m+v
+    }
+end
+
+# Count of distinct answers after intersecting all member's answers
+def part2 groups
+    yeses = groups.map { | g |
+        members = g.split("\n")
+        shared_answers = members.reduce("abcdefghijklmnopqrstuvwxyz".chars) { |m,v |
+            m & v.chars
+        }.uniq.length
+    }
+
+    return yeses.reduce(0){ |m,v|
+        m+v
+    }
+end
+
+puts part1(groups)
+puts part2(groups)
 
