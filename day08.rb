43cac1d53a20d366da58a9167ff0108f0847023e
David Edgar <david.edgar@bigtincan.com>
2020-12-09 18:35:16 +1100

    Refactoring continues

--- day08.rb
+++ day08.rb
@@ -21,43 +21,85 @@ class Instruction
     end
 end
 
-class Computer
-    attr_accessor :accumulator, :instruction, :last_instruction, :executed
+def NoOp state, instruction
+    return State.new(
+        state.accumulator, 
+        state.instruction_pointer+1,
+        state.executed.merge({state.instruction_pointer=>true}))
+end
 
-    def initialize
+def Jump state, instruction
+    return State.new(
+        state.accumulator, 
+        state.instruction_pointer+instruction.value,
+        state.executed.merge({state.instruction_pointer=>true})
+    )
+end
 
+def Accumulate state, instruction
+    return State.new(
+        state.accumulator+instruction.value,
+        state.instruction_pointer+1,
+        state.executed.merge({state.instruction_pointer=>true})
+    )
+end
+
+class State
+    attr_accessor :accumulator, :instruction_pointer, :executed
+    
+    def initialize acc, ip, executed
+        @accumulator = acc
+        @instruction_pointer = ip
+        @executed = executed
     end
 
-    def process instructions, allow_corruption_fixes = false
+    def has_instruction_been_processed? instruction=nil
+        @executed[instruction || @instruction_pointer]
+    end
+end
+
+class Computer
+    attr_accessor :state
+
+    def initialize
+        reset
+    end
+
+    def reset
         @executed = {}
         @accumulator = 0
         @instruction = 0
         @last_instruction = 0
 
-        while @instruction < instructions.length
-            i = instructions[@instruction]
-            break if @executed[@instruction]
-            @last_instruction = @instruction
+    end
 
+    def exec instructions, allow_corruption_fixes = false
+        state = State.new(0,0, {})
+
+        while state.instruction_pointer < instructions.length
+            i = instructions[state.instruction_pointer]
+            break if state.has_instruction_been_processed?
+            
             if i.command ==  "nop"
-                if allow_corruption_fixes && has_next_instruction_been_seen?(@instruction+1)
-                    @instruction += i.value
-                else
-                    @instruction+=1
+                next_state = NoOp(state,i)
+                if allow_corruption_fixes && next_state.has_instruction_been_processed?
+                    next_state = Jump(state,i)
                 end
+
+                state = next_state
             elsif i.command == "acc"
-                @accumulator += i.value
-                @instruction+=1
+                state = Accumulate(state,i)
             elsif i.command == "jmp"
-                if allow_corruption_fixes && has_next_instruction_been_seen?(@instruction + i.value)
-                    @instruction+=1
-                else
-                    @instruction += i.value
+                next_state = Jump(state,i)
+                if allow_corruption_fixes && next_state.has_instruction_been_processed?
+                    next_state = NoOp(state,i)
                 end
-                
+
+                state = next_state
             end
-            @executed[@last_instruction] = true
         end
+
+        return state
     end
 
     def has_next_instruction_been_seen? i
@@ -72,10 +114,11 @@ instructions = data.map{|l|
 
 #Part 1
 c = Computer.new
-c.process instructions
-puts c.accumulator
+state = c.exec instructions
+puts state.accumulator
+
 
 # Part 2
-c = Computer.new
-c.process instructions, true
-puts c.accumulator
+c.reset
+state = c.exec instructions, true
+puts state.accumulator
