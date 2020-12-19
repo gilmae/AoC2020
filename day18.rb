c5b620ddd1b1491c65e922bda6df0d69b4d35661
gilmae <avocadia@fastmail.fm>
2020-12-19 16:01:39 +1100

    Complete 2020.18

--- day18.rb
+++ day18.rb
@@ -80,3 +80,92 @@ c = Calculator.new
 puts data.map {|f|
   c.calculate f
 }.sum
+
+class CalculatorB
+  def initialize
+    @buffer = nil
+    @operator_buffer = nil
+  end
+
+  def calculate formula
+    parts = parse formula
+
+    after_first_pass = []
+    while parts.length > 0
+      p = parts.shift
+      case p
+      when "*",  Integer, Array
+        after_first_pass.push p
+      when "+"
+        operand1 = after_first_pass.pop
+        after_first_pass.push(operand1+parts.shift)
+      end
+    end
+
+    buffer = 0
+    operand = nil
+    while after_first_pass.length > 0
+      p = after_first_pass.shift
+      case p
+      when Integer
+        buffer = p
+      when "*"
+        buffer = buffer * after_first_pass.shift
+      end
+
+    end
+
+    buffer
+  end
+
+  def parse formula
+    buffer = []
+    read_buffer = ''
+    clause_buffer = ''
+
+    abc = 0
+
+    chars = formula.chars
+    index = 0
+    while index < chars.length
+      c = chars[index]
+      case c
+      when ' '
+        if (read_buffer || '') != ''
+          buffer.push read_buffer.to_i
+          read_buffer = ''
+        end
+      when '('
+        clause_buffer = ''
+        abc +=1
+        clause_buffer += c
+        index+=1
+        while index < chars.length && abc > 0
+          clause_buffer += chars[index]
+          abc +=1 if chars[index] == "("
+          abc -=1 if chars[index] == ")"
+          index +=1
+        end
+        
+        buffer.push calculate(clause_buffer[1..-2])
+      when '+', "*"
+        buffer.push c
+      else
+        read_buffer += c
+      end
+
+      index +=1
+    end
+
+    if (read_buffer || '') != ''
+      buffer.push read_buffer.to_i
+    end
+
+    buffer
+  end
+end
+
+c = CalculatorB.new
+p data.map {|f|
+  c.calculate f
+ }.sum
