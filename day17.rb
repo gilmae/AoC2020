5d797a5c8206133737f765b668e0a6bd469b5a29
gilmae <avocadia@fastmail.fm>
2020-12-21 17:14:22 +1100

    Complete 2020.17.2, but slooooowly

--- day17.rb
+++ day17.rb
@@ -19,94 +19,221 @@ data.each_with_index {|line, y|
   }
 }
 
-def get_neighbours x,y,z, cells
-  neighbours = []
-  (-1..1).each {|xd|
-    (-1..1).each{|yd|
-      (-1..1).map{|zd|
-        next if xd==0 && yd==0 && zd==0
-        neighbours.push([x+xd,y+yd,z+zd]) if cells[[x+xd, y+yd,z+zd]]
+class ThreeDimenionalBoard
+  attr_reader :cells
+  def initialize cells
+    @cells = cells
+    @generations = []
+  end
+
+  def get_neighbours x,y,z
+    neighbours = []
+    (-1..1).each {|xd|
+      (-1..1).each{|yd|
+        (-1..1).map{|zd|
+          next if xd==0 && yd==0 && zd==0
+          neighbours.push([x+xd,y+yd,z+zd]) if @cells[[x+xd, y+yd,z+zd]]
+        }
       }
     }
-  }
-  neighbours
-end
+    neighbours
+  end
 
 
-def get_next_generation cells
-  next_state = Hash.new(false)
+  def get_next_generation
+    next_state = Hash.new(false)
 
-  xmin,xmax = cells.keys.map{|k|k[0]}.minmax
-  ymin,ymax = cells.keys.map{|k|k[1]}.minmax
-  zmin,zmax = cells.keys.map{|k|k[2]}.minmax
+    xmin,xmax = @cells.keys.map{|k|k[0]}.minmax
+    ymin,ymax = @cells.keys.map{|k|k[1]}.minmax
+    zmin,zmax = @cells.keys.map{|k|k[2]}.minmax
   
-  (xmin-1..xmax+1).each { |nx|
-    (ymin-1..ymax+1).each { |ny|
-      (zmin-1..zmax+1).each { |nz|
-        neighbours = get_neighbours nx,ny,nz, cells
-        case cells[[nx,ny,nz]]
-        when true
-          next_state[[nx,ny,nz]] = (neighbours.length==2 || neighbours.length==3)
-        else
-          next_state[[nx,ny,nz]] = neighbours.length==3
-        end
+    (xmin-1..xmax+1).each { |nx|
+      (ymin-1..ymax+1).each { |ny|
+        (zmin-1..zmax+1).each { |nz|
+          neighbours = get_neighbours nx,ny,nz
+          case @cells[[nx,ny,nz]]
+          when true
+            next_state[[nx,ny,nz]] = true if (neighbours.length==2 || neighbours.length==3)
+          else
+            next_state[[nx,ny,nz]] = true if neighbours.length==3
+          end
+        }
       }
     }
-  }
 
-  next_state
-end
+    next_state
+  end
 
-def print_generation cells
-  xmin,xmax = cells.keys.map{|k|k[0]}.minmax
-  ymin,ymax = cells.keys.map{|k|k[1]}.minmax
-  zmin,zmax = cells.keys.map{|k|k[2]}.minmax
+  def print_generation
+    xmin,xmax = @cells.keys.map{|k|k[0]}.minmax
+    ymin,ymax = @cells.keys.map{|k|k[1]}.minmax
+    zmin,zmax = @cells.keys.map{|k|k[2]}.minmax
   
-  (zmin..zmax).each { |z|
-    puts "\nz=#{z}"
-    (ymin..ymax).each { |y|
-      row = ""
-      (xmin..xmax).each { |x|
-        row += cells[[x,y,z]]?"#":"."
-      }
-      puts row
+    (zmin..zmax).each { |z|
+      puts "\nz=#{z}"
+      (ymin..ymax).each { |y|
+        row = ""
+        (xmin..xmax).each { |x|
+          row += @cells[[x,y,z]]?"#":"."
+        }
+        puts row
 
-    }
-  }
-end
-
-def get_active_cells cells
-  active = []
-  xmin,xmax = cells.keys.map{|k|k[0]}.minmax
-  ymin,ymax = cells.keys.map{|k|k[1]}.minmax
-  zmin,zmax = cells.keys.map{|k|k[2]}.minmax
-  
-  (zmin..zmax).each { |z|
-    (ymin..ymax).each { |y|
-      (xmin..xmax).each { |x|
-        active.push [x,y,z] if cells[[x,y,z]]
       }
     }
-  }
-  active
+  end
+
+  def get_active_cells
+    active = []
+    xmin,xmax = @cells.keys.map{|k|k[0]}.minmax
+    ymin,ymax = @cells.keys.map{|k|k[1]}.minmax
+    zmin,zmax = @cells.keys.map{|k|k[2]}.minmax
+  
+    (zmin..zmax).each { |z|
+      (ymin..ymax).each { |y|
+        (xmin..xmax).each { |x|
+          active.push [x,y,z] if @cells[[x,y,z]]
+        }
+      }
+    }
+    active
+  end
+
+  def get_board_size
+  
+    xmin,xmax = @cells.keys.map{|k|k[0]}.minmax
+    ymin,ymax = @cells.keys.map{|k|k[1]}.minmax
+    zmin,zmax = @cells.keys.map{|k|k[2]}.minmax
+  
+    (xmin..xmax).to_a.length * (xmin..ymax).to_a.length * (zmin..zmax).to_a.length
+  end
+
+  def tick
+    @cells = get_next_generation
+  end
 end
 
-def get_board_size cells
-  
-  xmin,xmax = cells.keys.map{|k|k[0]}.minmax
-  ymin,ymax = cells.keys.map{|k|k[1]}.minmax
-  zmin,zmax = cells.keys.map{|k|k[2]}.minmax
-  
-  (xmin..xmax).to_a.length * (xmin..ymax).to_a.length * (zmin..zmax).to_a.length
-end
-
-states = [cells]
+board = ThreeDimenionalBoard.new(cells)
 (1..6).each {|i|
-  states.push(get_next_generation(states.last))
+  board.tick
 }
 
-puts get_active_cells(states.last).length
-puts get_board_size(states.last)
+puts board.get_active_cells.length
+puts board.get_board_size
 
+class FourDimenionalBoard
+  attr_reader :cells
+  def initialize cells
+    @cells = cells
+    @generations = []
+  end
+
+  def get_neighbours x,y,z, w
+    neighbours = []
+    (-1..1).each {|xd|
+      (-1..1).each{|yd|
+        (-1..1).map{|zd|
+          (-1..1).map{|wd|
+            next if xd==0 && yd==0 && zd==0 && wd==0
+            neighbours.push([x+xd,y+yd,z+zd, w+wd]) if @cells[[x+xd, y+yd,z+zd, w+wd]]
+          }
+        }
+      }
+    }
+    neighbours
+  end
+
+  def get_next_generation
+    next_state = Hash.new(false)
+
+    xmin,xmax = @cells.keys.map{|k|k[0]}.minmax
+    ymin,ymax = @cells.keys.map{|k|k[1]}.minmax
+    zmin,zmax = @cells.keys.map{|k|k[2]}.minmax
+    wmin,wmax = @cells.keys.map{|k|k[3]}.minmax
+  
+    (xmin-1..xmax+1).each { |nx|
+      (ymin-1..ymax+1).each { |ny|
+        (zmin-1..zmax+1).each { |nz|
+          (wmin-1..wmax+1).each { |nw|
+            neighbours = get_neighbours nx,ny,nz, nw
+            case @cells[[nx,ny,nz,nw]]
+            when true
+              next_state[[nx,ny,nz,nw]] = true if (neighbours.length==2 || neighbours.length==3)
+            else
+              next_state[[nx,ny,nz,nw]] = true if neighbours.length==3
+            end
+          }
+        }
+      }
+    }
+
+    next_state
+  end
+
+  # def print_generation
+  #   xmin,xmax = @cells.keys.map{|k|k[0]}.minmax
+  #   ymin,ymax = @cells.keys.map{|k|k[1]}.minmax
+  #   zmin,zmax = @cells.keys.map{|k|k[2]}.minmax
+  
+  #   (zmin..zmax).each { |z|
+  #     puts "\nz=#{z}"
+  #     (ymin..ymax).each { |y|
+  #       row = ""
+  #       (xmin..xmax).each { |x|
+  #         row += @cells[[x,y,z]]?"#":"."
+  #       }
+  #       puts row
+
+  #     }
+  #   }
+  # end
+
+  def get_active_cells
+    active = []
+    xmin,xmax = @cells.keys.map{|k|k[0]}.minmax
+    ymin,ymax = @cells.keys.map{|k|k[1]}.minmax
+    zmin,zmax = @cells.keys.map{|k|k[2]}.minmax
+    wmin,wmax = @cells.keys.map{|k|k[3]}.minmax
+  
+    (wmin..wmax).each { |w|
+      (zmin..zmax).each { |z|
+        (ymin..ymax).each { |y|
+          (xmin..xmax).each { |x|
+            active.push [x,y,z,w] if @cells[[x,y,z,w]]
+          }
+        }
+      }
+    }
+    active
+  end
+
+  def get_board_size
+  
+    xmin,xmax = @cells.keys.map{|k|k[0]}.minmax
+    ymin,ymax = @cells.keys.map{|k|k[1]}.minmax
+    zmin,zmax = @cells.keys.map{|k|k[2]}.minmax
+    wmin,wmax = @cells.keys.map{|k|k[3]}.minmax
+  
+    (xmin..xmax).to_a.length * (xmin..ymax).to_a.length * (zmin..zmax).to_a.length * (wmin..wmax).to_a.length
+  end
+
+  def tick
+    @cells = get_next_generation
+  end
+end
+
+fourDCells = Hash.new(false)
+data.each_with_index {|line, y|
+  line.chomp.split("").each_with_index {|c, x|
+  fourDCells[[x,y,0,0]] = (c == "#")
+  }
+}
+
+board = FourDimenionalBoard.new(fourDCells)
+(1..6).each {|i|
+  board.tick
+}
+
+puts board.get_active_cells.length
+puts board.get_board_size
 
 
